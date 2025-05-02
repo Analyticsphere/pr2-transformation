b04b02211df0af95701922e5578aac4b06179cfe
Jake Peters <jcbptrs@gmail.com>
2025-05-02 09:08:36 -0400

    add unit tests for loop variable functions

--- test/test_utils.py
+++ test/test_utils.py
@@ -10,10 +10,12 @@ from core.utils import extract_ordered_concept_ids, extract_loop_number, group_v
     ("D_123412349_1_1_D_987654321_1_1", ["123412349", "987654321"]),
     ("d_999999999", ["999999999"]),
     ("D_812370563_1_1_D_812370563_1_1_D_665036297", ["812370563", "812370563", "665036297"]),
+    ("D_812370563_1_1_D_812370563_V3_1_1_D_665036297", ["812370563", "812370563", "665036297"]),
     ("random_text", []),  # No concept ID should return an empty list
 ])
 def test_extract_ordered_concept_ids(var_name, expected):
     assert extract_ordered_concept_ids(var_name) == expected
+
 
 # Test cases for extract_loop_number
 @pytest.mark.parametrize("var_name, expected", [
@@ -22,6 +24,8 @@ def test_extract_ordered_concept_ids(var_name, expected):
     ("d_111111111_1_1_d_222222222_1_1", 1),
     ("d_123456789_9_9_d_987654321_9_9", 9),
     ("d_123456789_9_9_d_987654321_9_9_9_9_9_9", 9),
+    ("d_123456789_9_9_d_987654321_v1_9_9_9_9_9_9", 9),
+    ("d_123456789_v3_9_9_d_987654321_9_9_9_9_9_9", 9),
     ("d_123456789_5_5", 5),
     ("d_123456789", None),  # No loop number, should return None
     ("d_111111111_12_12_d_222222222_12_12", 12),
@@ -29,6 +33,7 @@ def test_extract_ordered_concept_ids(var_name, expected):
 
 def test_extract_loop_number(var_name, expected):
     assert extract_loop_number(var_name) == expected
+
 
 # Test cases for group_by_concept_and_loop
 def test_group_vars_by_cid_and_loop_num():
